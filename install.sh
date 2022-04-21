5b31d3781ceca8b94f3f30b2635f11354caf4dab
Mat Sachs <matochondrion@gmail.com>
2022-04-21 16:03:23 -0700

    Break combined pkgs install into separate commands

--- install.sh
+++ install.sh
@@ -5,20 +5,19 @@ curl -L https://nixos.org/nix/install | sh
 . ~/.nix-profile/etc/profile.d/nix.sh
 
 # install packages
-nix-env -iA \
-	nixpkgs.zsh \
-	nixpkgs.antibody \
-	nixpkgs.git \
-	nixpkgs.neovim \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.yarn \
-	nixpkgs.fzf \
-	nixpkgs.ripgrep \
-	nixpkgs.bat \
-	nixpkgs.gnumake \
-	nixpkgs.gcc \
-	nixpkgs.direnv
+nix-env -iA nixpkgs.zsh
+nix-env -iA nixpkgs.antibody
+nix-env -iA nixpkgs.git
+nix-env -iA nixpkgs.neovim
+nix-env -iA nixpkgs.tmux
+nix-env -iA nixpkgs.stow
+nix-env -iA nixpkgs.yarn
+nix-env -iA nixpkgs.fzf
+nix-env -iA nixpkgs.ripgrep
+nix-env -iA nixpkgs.bat
+nix-env -iA nixpkgs.gnumake
+nix-env -iA nixpkgs.gcc
+nix-env -iA nixpkgs.direnv
 
 # stow dotfiles
 stow git
